e455e81d6485dc445fa587c09daaba4b58e8f36a
Eros000 <e.ghilardi@hotmail.it>
2023-06-12 15:47:25 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const km = prompt('Quanti chilometri intende percorrere?')
+
+console.log ('km', km, typeof km)
+
+const age = prompt('Quanti anni hai?')
+
+console.log ('age', age, typeof age)
+
+const price = (km*0.21)
+
+console.log ('price', price, typeof price)
+
