0c6eb2df554d2c7f9e816aab7b05a466ba035159
Eros000 <e.ghilardi@hotmail.it>
2023-06-12 21:31:20 +0200

    Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -6,7 +6,20 @@ const age = prompt('Quanti anni hai?')
 
 console.log ('age', age, typeof age)
 
-const price = (km*0.21)
+const ageNumber = parseInt (age)
+
+let price = (km*0.21)
+
+if (ageNumber < 18){
+    price =(km*(0.21-(0.21*20/100)))
+}
+
+if (ageNumber > 65){
+    price =(km*(0.21-(0.21*40/100)))
+}
 
 console.log ('price', price, typeof price)
 
+const Result = document.getElementById ('my-price')
+
+Result.innerHTML = price
